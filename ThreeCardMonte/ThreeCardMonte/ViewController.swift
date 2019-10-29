c39539353c2b2072b8b712f50660a0c10aaca8fe
Jaheed Haynes <jaheedhaynes@pursuit.org>
2019-10-29 15:26:51 -0400

    buttons work, still not able to show King Card when wrong selection is made

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -10,11 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    @IBOutlet weak var Card1: UIButton!
+    @IBOutlet weak var Card2: UIButton!
+    @IBOutlet weak var Card3: UIButton!
+    
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+//---------------------------------------
+    
+    let card = [0,1,2]
+    
+    
+    @IBAction func cardIndex1(_ sender: UIButton) {
+        
+        switch sender.tag {
+        case 0:
+            let randomCard = card.randomElement()
+            if randomCard == 1 {
+                Card1.setBackgroundImage(#imageLiteral(resourceName: "kingCard.jpg"), for: .normal)
+                mainLabel.text = "You Win"
+                
+            } else {
+                Card1.setBackgroundImage(#imageLiteral(resourceName: "threeCard.png"), for: .normal)
+                mainLabel.text = "You Lose"
+            }
+        case 1:
+            let randomCard = card.randomElement()
+            if randomCard == 1 {
+                Card2.setBackgroundImage(#imageLiteral(resourceName: "kingCard.jpg"), for: .normal)
+                mainLabel.text = "You Win"
+            } else {
+                Card2.setBackgroundImage(#imageLiteral(resourceName: "threeCard.png"), for: .normal)
+                mainLabel.text = "You Lose"
+            }
+        case 2:
+            let randomCard = card.randomElement()
+            if randomCard == 1 {
+                Card3.setBackgroundImage(#imageLiteral(resourceName: "kingCard.jpg"), for: .normal)
+                mainLabel.text = "You Win"
+            } else {
+                Card3.setBackgroundImage(#imageLiteral(resourceName: "threeCard.png"), for: .normal)
+                mainLabel.text = "You Lose"
+            }
+            
+        default:
+            break
+        }
+    }
+    
+//---------------------------------------
+    
+    @IBAction func newGame(_ sender: UIButton) {
+        mainLabel.text = "3 Card Montie"
+        Card1.setBackgroundImage(#imageLiteral(resourceName: "cardBackRed.png"), for: .normal)
+        Card2.setBackgroundImage(#imageLiteral(resourceName: "cardBackRed.png"), for: .normal)
+        Card3.setBackgroundImage(#imageLiteral(resourceName: "cardBackRed.png"), for: .normal)
+    }
 }
+
 
